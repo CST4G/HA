309bd47f6b24dbc74c9e696a49b6e8fcbea851da
Mark Niehe <mniehe@gmaill.com>
2014-02-01 15:49:53 -0800

    code cleanup

--- healthApp/Models/Accounts.cs
+++ healthApp/Models/Accounts.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Data;
 using System.Data.Entity;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
 
 namespace healthApp.Models {
     public class Accounts {
@@ -35,35 +30,6 @@ namespace healthApp.Models {
             Accounts user = db.Accounts.SingleOrDefault( account => account.Username == _username && account.Password == _password );
 
             return (!(user == null));
-
-            /*
-            using (var cn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=H:\Login\Login\App_Data\database.mdf;Integrated Security=True")) 
-            {
-                string _sql = @"SELECT [Username] FROM [dbo].[Accounts] " +
-                       @"WHERE [Username] = @u AND [Password] = @p";
-                var cmd = new SqlCommand(_sql, cn);
-                cmd.Parameters
-                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
-                    .Value = _username;
-                cmd.Parameters
-                    .Add(new SqlParameter("@p", SqlDbType.NVarChar))
-                    .Value = (_password);
-                cn.Open();
-                var reader = cmd.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    reader.Dispose();
-                    cmd.Dispose();
-                    return true;
-                }
-                else
-                {
-                    reader.Dispose();
-                    cmd.Dispose();
-                    return false;
-                }
-            }
-             * */
         }
 
         public static string findType( string _username, string _password, AccountsDBContext db ) {
@@ -71,45 +37,10 @@ namespace healthApp.Models {
             Accounts user = db.Accounts.SingleOrDefault( account => account.Username == _username );
 
             return user.acctType;
-
-            /*
-            using (var cn = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=H:\Login\Login\App_Data\database.mdf;Integrated Security=True"))
-            {
-                string _sql = @"SELECT [acctType] FROM [dbo].[Accounts] " +
-                       @"WHERE [Username] = @u AND [Password] = @p";
-                var cmd = new SqlCommand(_sql, cn);
-
-                
-                cmd.Parameters
-                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
-                    .Value = _username;
-                cmd.Parameters
-                    .Add(new SqlParameter("@p", SqlDbType.NVarChar))
-                    .Value = (_password);
-                
-                cn.Open();
-                var reader = cmd.ExecuteReader();
-
-                int i =0;
-                string test = "";
-                while (reader.Read())
-                {
-                    test += reader.GetString(i++);
-                }
-                return test;
-            }
-             * */
         }
 
     }
 
-
-    /*
-    public class AccountsDBContext : DbContext
-    {
-        public DbSet<Accounts> Accounts { get; set; }
-    }
-    */
     public class ApplicationUser : IdentityUser {
     }
 
