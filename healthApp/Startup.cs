30159aefe3a0d5ac031e55048de92b7c15137aa1
chapmanc <chapman.chris.h@gmail.com>
2014-02-02 16:35:55 -0800

    partial view with Chat

--- healthApp/Startup.cs
+++ healthApp/Startup.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.Owin;
-using Owin;
-
-[assembly: OwinStartupAttribute(typeof(healthApp.Startup))]
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(healthApp.Startup))]
 namespace healthApp
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
         }
     }
 }
